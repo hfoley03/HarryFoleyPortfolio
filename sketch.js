92c1994d395e60b3b28b884743693161a100dcba
“Harry <hfoley03@gmail.com>
2023-12-03 14:49:11 +0100

    Initial commit

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function setup() {
 }
 
 function draw() {
-  background(22,22, 22); 
+  background(0,0, 0); 
   noStroke();
   ellipse(mouseX, mouseY, 200, 200);
   ellipse(windowWidth - mouseX, windowHeight -mouseY, 200, 200);
